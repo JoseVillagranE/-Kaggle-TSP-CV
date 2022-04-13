f088b88203865522eb9e3a5bdaa6eb95defc749f
Jose <josevillagran18@gmail.com>
2022-04-12 23:47:08 -0400

    Added validation to the pipeline

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 from asyncio import base_tasks
+import numpy as np
 import torch
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision.transforms import Resize, Compose, ToTensor
 
 from tsp_cv_dataset import TSPCV_Dataset
 from cnn import cnn
 
-
-def main(cfg):
+def training(cfg):
 
     transform = Compose([Resize(cfg["size"]), ToTensor()])
 
-    train_dataset = TSPCV_Dataset(cfg["train_path"], transform)
-    val_dataset = TSPCV_Dataset(cfg["test_path"], transform)
+    dataset = TSPCV_Dataset(cfg["train_path"], transform)
+    dataset_size = len(dataset)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=cfg["batch_size"])
-    test_dataloader = DataLoader(val_dataset, batch_size=cfg["batch_size"])
+    indices = list(range(dataset_size))
+    split = int(np.floor(cfg["validation_split"] * dataset_size))
+    
+    np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
+
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+
+    train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=cfg["batch_size"], 
+                                               sampler=train_sampler)
+    validation_dataloader = torch.utils.data.DataLoader(dataset, batch_size=cfg["batch_size"],
+                                                    sampler=valid_sampler)
 
     model = cnn(lr=cfg["lr"], eps=cfg["eps"], wd=cfg["wd"])
 
@@ -36,10 +48,12 @@ def main(cfg):
         logger= wandb_logger
     )
 
-    trainer.fit(model, train_dataloader)
+    trainer.fit(model, train_dataloader, validation_dataloader)
 
-    trainer.test(dataloaders=test_dataloader)
 
+# def testing(cfg):
+
+#     trainer.test(dataloaders=test_dataloader)
 
 if __name__ == "__main__":
 
@@ -50,9 +64,9 @@ if __name__ == "__main__":
         "lr": 1e-3,
         "max_epochs": 10,
         "size": (329, 200),
-        "test_path": "data/test.csv",
         "train_path": "data/train.csv",
         "val_every_n": 2,
+        "validation_split": 0.2,
         "wd": 1e-5,
     }
 
